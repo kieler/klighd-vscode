2b51883e9587d3630ec6a1a9c31c411f1665e3f3
sdo <stu114053@mail.uni-kiel.de>
2018-12-18 10:10:34 +0100

    Keith: Modify kieler.ini via sh for linux

---
+++ script.sh
@@ -0,0 +1,4 @@
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sed -i '1s/^/-application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' ../../../language-server/kieler/kieler.ini
+fi
