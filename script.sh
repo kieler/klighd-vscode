a70d4ba5bd351b94e0e9827671e65b330448d782
sdo <stu114053@mail.uni-kiel.de>
2018-12-18 12:17:12 +0100

    Keith: Test shell script for linux

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i '1s/^/-application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' ../../../language-server/kieler/kieler.ini
+sed -i '1s/^/-application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' ./language-server/kieler/kieler.ini
