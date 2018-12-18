3fe3331018e875fbca2919817f43258e6623ae0c
sdo <stu114053@mail.uni-kiel.de>
2018-12-18 13:53:50 +0100

    Keith: Removed copy of pragmatic style files, Added description for shs

--- cpy-highlighting.sh
+++ cpy-highlighting.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
+# Copy all highlighting files in semantic plugin that are named XXX-monaco-language.ts in the corresponding Theia extension
 find ./semantics/plugins -name '*monaco-language.ts' -exec cp {} ./semantics/theia/extension/keith-language/src/browser/ \;
-find ./pragmatics/plugins -name '*monaco-language.ts' -exec cp {} ./semantics/theia/extension/keith-language/src/browser/ \;
--- script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i '1s/^/-application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' ./language-server/kieler.ini
