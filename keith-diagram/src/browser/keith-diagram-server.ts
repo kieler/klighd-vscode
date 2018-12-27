ddb17850e367d030b09d3acda2abdd05c6ea025d
Niklas Rentz <stu114054@informatik.uni-kiel.de>
2018-12-27 12:51:26 +0100

    Diagramoptions-view: now opens after a new model in  diagram view opens

--- keith-diagram/src/browser/keith-diagram-server.ts
+++ keith-diagram/src/browser/keith-diagram-server.ts
@@ -1,13 +1,25 @@
 import { injectable } from "inversify";
 import { TheiaDiagramServer } from "theia-sprotty/lib";
-import { Action, ActionHandlerRegistry, ComputedBoundsAction } from "sprotty/lib";
+import { Action, ActionHandlerRegistry, ComputedBoundsAction, ActionMessage, SetModelCommand } from "sprotty/lib";
 import { ComputedTextBoundsAction, RequestTextBoundsCommand } from "keith-sprotty/lib/actions";
+import { Emitter, Event } from "@theia/core";
 
 /**
  * This class extends the Theia diagram Server to also handle the Request- and ComputedTextBoundsAction
  */
 @injectable()
 export class KeithDiagramServer extends TheiaDiagramServer {
+
+    protected readonly onModelUpdatedEmitter = new Emitter<string>()
+
+    public readonly onModelUpdated: Event<string> = this.onModelUpdatedEmitter.event
+
+    messageReceived(message: ActionMessage) {
+        super.messageReceived(message)
+        if (message.action.kind === SetModelCommand.KIND) {
+            this.onModelUpdatedEmitter.fire(message.clientId)
+        }
+    }
 
     handleLocally(action: Action): boolean {
         switch (action.kind) {
