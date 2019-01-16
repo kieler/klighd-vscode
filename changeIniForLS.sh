dcd3e5bdd32d3ead5c43fb0c5c6c74c27f0ecf69
Soeren Domroes <sdo@informatik.uni-kiel.de>
2019-01-16 14:45:16 +0100

    Keith-product: Added command to edit kieler.ini for macOs.

--- changeIniForLS.sh
+++ changeIniForLS.sh
@@ -4,4 +4,26 @@
 # -application
 # de.cau.cs.kieler.language.server.LanguageServer
 #--noSplash
-sed -i '1s/^/-application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' ./language-server/kieler.ini
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    # Linux
+    sed -i '1s/^/-application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' ./language-server/kieler.ini
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    sed -i '1s/^/
+    -application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' ./language-server/Eclipse/kieler.ini
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    echo not supported
+        # POSIX compatibility layer and Linux environment emulation for Windows
+elif [[ "$OSTYPE" == "msys" ]]; then
+    echo not supported
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+elif [[ "$OSTYPE" == "win32" ]]; then
+    echo not supported
+        # I'm not sure this can happen.
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    echo not supported
+        # ...
+else
+    echo not supported
+        # Unknown.
+fi
