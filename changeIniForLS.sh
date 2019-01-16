d75312a81ffda56e54ea5576981267e8c878f8c8
Soeren Domroes <sdo@informatik.uni-kiel.de>
2019-01-16 15:15:33 +0100

    Keith-product: Changed script to modify macOs kieler.ini.

--- changeIniForLS.sh
+++ changeIniForLS.sh
@@ -9,8 +9,9 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sed -i '1s/^/-application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' ./language-server/kieler.ini
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
-    sed -i '1s/^/
-    -application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' ./language-server/Eclipse/kieler.ini
+    sed -i.bu '1s/^/
+    -application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' ./language-server/Eclipse/kieler.ini &&
+    rm ./language-server/Eclipse/kieler.ini.bu
 elif [[ "$OSTYPE" == "cygwin" ]]; then
     echo not supported
         # POSIX compatibility layer and Linux environment emulation for Windows
