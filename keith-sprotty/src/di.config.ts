afb82134e0f8ddce3e9a648efd7d7679d05fe007
Niklas Rentz <nre@informatik.uni-kiel.de>
2019-01-18 16:07:21 +0100

    Added comment how to remove that  the HiddenBoundsUpdater is still executed.

--- keith-sprotty/src/di.config.ts
+++ keith-sprotty/src/di.config.ts
@@ -32,6 +32,16 @@ const kGraphDiagramModule = new ContainerModule((bind, unbind, isBound, rebind) 
 })
 
 const textBoundsModule = new ContainerModule((bind, unbind, isBound, rebind) => {
+    // TODO:
+    // This should really first unbind the RequestBoundsCommand from the TYPES.ICommand registry
+    // and the HiddenBoundsUpdater from the TYPES.HiddenVNodeDecorator registry, but inversify
+    // does not support such a feature.
+    // See the ticket https://github.com/inversify/InversifyJS/issues/1035
+    // I would like some syntax such as:
+    // unbind(Types.HiddenVNodeDecorator).from(HiddenBoundsUpdater)
+    // to remove only that specific binding, not all of the bindings registered for the Types.HiddenVNodeDecorator.
+    // With that, the HiddenBoundsUpdater should not be called anymore and not issue any CalculatedBoundsAction,
+    // which is currently only ignored by the overwritten handle method for that action in the KeithDiagramServer.
     bind(TYPES.ICommand).toConstructor(RequestTextBoundsCommand)
     bind(TYPES.HiddenVNodeDecorator).to(HiddenTextBoundsUpdater).inSingletonScope()
 });
