1ebf6a43ee41fc3ecb5733b454b65d44253189d4
Soeren Domroes <sdo@informatik.uni-kiel.de>
2019-01-21 14:59:40 +0100

    Keith-product: Try different mac sed \n representation.

--- changeIniForLS.sh
+++ changeIniForLS.sh
@@ -9,10 +9,13 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sed -i '1s/^/-application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' ./language-server/kieler.ini
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
-    sed -i.bu '1s/^/-application'$'\nde.cau.cs.kieler.language.server.LanguageServer'$'\n-noSplash'$'\n/' ./language-server/Contents/Eclipse/kieler.ini &&
+    sed -i.bu "1s/^/-application
+    de.cau.cs.kieler.language.server.LanguageServer
+    -noSplash
+    " ./language-server/Contents/Eclipse/kieler.ini &&
     rm ./language-server/Contents/Eclipse/kieler.ini.bu
 elif [[ "$OSTYPE" == "cygwin" ]]; then
-    sed -i '1s/^/-application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' "language-server\kieler.ini"
+    sed -i '1s/^/-application\r\nde.cau.cs.kieler.language.server.LanguageServer\r\n-noSplash\r\n/' "language-server\kieler.ini"
         # POSIX compatibility layer and Linux environment emulation for Windows
 elif [[ "$OSTYPE" == "msys" ]]; then
     echo not supported
