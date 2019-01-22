8c6b450422b2746c9e7845091eb05b0a6c524856
Soeren Domroes <sdo@informatik.uni-kiel.de>
2019-01-22 11:16:37 +0100

    Keith-product: Mac sed: Changed command to fit mac syntax (again).

--- changeIniForLS.sh
+++ changeIniForLS.sh
@@ -9,12 +9,11 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sed -i '1s/^/-application\nde.cau.cs.kieler.language.server.LanguageServer\n-noSplash\n/' ./language-server/kieler.ini
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
-    sed -i.bu '1i\
+    sed -i '' '1i\
     -application
     de.cau.cs.kieler.language.server.LanguageServer
     -noSplash
-    ' ./language-server/Contents/Eclipse/kieler.ini &&
-    rm ./language-server/Contents/Eclipse/kieler.ini.bu
+    ' ./language-server/Contents/Eclipse/kieler.ini
 elif [[ "$OSTYPE" == "cygwin" ]]; then
     sed -i '1s/^/-application\r\nde.cau.cs.kieler.language.server.LanguageServer\r\n-noSplash\r\n/' "language-server\kieler.ini"
         # POSIX compatibility layer and Linux environment emulation for Windows
