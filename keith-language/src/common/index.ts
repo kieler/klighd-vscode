f10485be3a822ab33e3a62637f794868918176e5
Soeren Domroes <sdo@informatik.uni-kiel.de>
2019-04-08 16:49:06 +0200

    language: Improved doc.

---
+++ keith-language/src/common/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Used in the KeithLanguageClientContribution and KeithLanguageServerContribution to identify the LS
+ */
+export const LS_ID = "keith"
+/**
+ * Used in the KeithLanguageClientContribution and KeithLanguageServerContribution to name the LS
+ */
+export const LS_NAME = "Keith"
