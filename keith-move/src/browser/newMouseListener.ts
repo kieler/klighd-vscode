06ab161cf39856d5e12367227f5815a198951409
Jette <klette-99@kabelmail.de>
2019-04-11 15:35:20 +0200

    eigener MouseListener

---
+++ keith-move/src/browser/newMouseListener.ts
@@ -0,0 +1,30 @@
+import { MoveMouseListener, SModelElement, Action, findParentByFeature, isMoveable, SRoutingHandle,
+    isCreatingOnDrag, SelectAllAction, edgeInProgressID, SelectAction, SwitchEditModeAction, edgeInProgressTargetHandleID } from "sprotty";
+
+export class NewMouseListener extends MoveMouseListener {
+    mouseDown(target: SModelElement, event: MouseEvent): Action[] {
+        const result: Action[] = [];
+        if (event.button === 0) {
+            const moveable = findParentByFeature(target, isMoveable);
+            const isRoutingHandle = target instanceof SRoutingHandle;
+            if (moveable !== undefined || isRoutingHandle || isCreatingOnDrag(target)) {
+                this.lastDragPosition = { x: event.pageX, y: event.pageY };
+            } else {
+                this.lastDragPosition = undefined;
+            }
+            this.hasDragged = false;
+            if (isCreatingOnDrag(target)) {
+                result.push(new SelectAllAction(false));
+                result.push(target.createAction(edgeInProgressID));
+                result.push(new SelectAction([edgeInProgressID], []));
+                result.push(new SwitchEditModeAction([edgeInProgressID], []));
+                result.push(new SelectAction([edgeInProgressTargetHandleID], []));
+                result.push(new SwitchEditModeAction([edgeInProgressTargetHandleID], []));
+            } else if (isRoutingHandle) {
+                result.push(new SwitchEditModeAction([target.id], []));
+            }
+        }
+        console.log("hallo");
+        return result;
+    }
+}
