9a34dd2b13aecdb11d70c37bc42f3495f6fcd7a2
Soeren Domroes <sdo@informatik.uni-kiel.de>
2019-05-20 10:54:26 +0200

    kicool: Safe last compiled uri.

--- keith-kicool/src/browser/compiler-widget.tsx
+++ keith-kicool/src/browser/compiler-widget.tsx
@@ -122,6 +122,12 @@ export class CompilerWidget extends ReactWidget implements StatefulWidget {
      */
     public lastInvokedCompilation: string = ""
 
+    /**
+     * Holds the uri of the last compiled model, if any.
+     * This is used by the simulation to decide whether a simulation can be restarted.
+     */
+    public lastCompiledUri: string = ""
+
     /*
      * Indicates whether a compilation is currently running.
      * Enables to stop compilation button.
