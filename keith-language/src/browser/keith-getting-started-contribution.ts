7f3383c9c4085ff21791a279fa729a805566c133
Soeren Domroes <sdo@informatik.uni-kiel.de>
2019-08-14 15:23:28 +0200

    langauge: Getting started widget for keith, initial draft.

---
+++ keith-language/src/browser/keith-getting-started-contribution.ts
@@ -0,0 +1,118 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://rtsys.informatik.uni-kiel.de/kieler
+ *
+ * Copyright 2019 by
+ * + Kiel University
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ *
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ */
+
+import { GettingStartedContribution } from '@theia/getting-started/lib/browser/getting-started-contribution'
+import { injectable, inject } from 'inversify';
+import { Command, CommandRegistry, MessageService } from '@theia/core';
+import { Workspace } from '@theia/languages/lib/browser';
+import { FileSystem, FileSystemUtils, FileStat } from '@theia/filesystem/lib/common';
+import URI from '@theia/core/lib/common/uri';
+import { OpenerService, WidgetManager } from '@theia/core/lib/browser';
+import { open } from '@theia/core/lib/browser/opener-service'
+import { EditorManager } from '@theia/editor/lib/browser';
+
+export const OPEN_EXAMPLE_SCCHART: Command = {
+    id: 'open-example-scchart',
+    label: 'Open example SCChart',
+    category: 'KEITH'
+}
+
+@injectable()
+export class KeithGettingStartedContribution extends GettingStartedContribution {
+
+    @inject(Workspace) protected readonly workspace: Workspace
+    @inject(FileSystem) protected readonly fileSystem: FileSystem
+    @inject(OpenerService) protected readonly openerService: OpenerService;
+    @inject(EditorManager) protected readonly editorManager: EditorManager
+    @inject(WidgetManager) protected readonly widgetManager: WidgetManager
+    @inject(MessageService) protected readonly messageService: MessageService
+
+    fileUri = ""
+
+    registerCommands(registry: CommandRegistry): void {
+        registry.registerCommand(OPEN_EXAMPLE_SCCHART, {
+            execute: async () => {
+                const rootUri = this.workspace.rootUri
+                if (rootUri) {
+                    const parent: FileStat | undefined = await this.fileSystem.getFileStat(rootUri)
+                    if (parent !== undefined) {
+                        // Generate a unique file with preferred name Abro.sctx, if this name is taken it will be Abro_2.sctx, ...
+                        const fileUri = FileSystemUtils.generateUniqueResourceURI(new URI(parent.uri), parent, 'Abro', '.sctx');
+                        await this.fileSystem.createFile(fileUri.toString(), { content: this.abroText})
+                        // Open the created file
+                        await open(this.openerService, fileUri)
+                        // Magic string ahead!!
+                        // This opens the diagram widget.
+                        registry.executeCommand('diagram.open')
+                    } else {
+                        this.messageService.error("There seems to be a problem with the created workspace.")
+                    }
+                } else {
+
+                    await this.messageService.info("No workspace was open. Open workspace on home of current user.")
+                    const fileStat = await this.fileSystem.getCurrentUserHome()
+                    if (fileStat) {
+                        const uri = fileStat.uri
+                        console.log(uri)
+                        await this.workspaceService.open(new URI(uri))
+                    }
+                }
+            }
+        })
+    }
+
+    // TODO complete this docu.
+    abroText = `// ABRO Tutorial
+// This is the declaration of an SCChart
+scchart ABRO {
+    // Variable declaration:
+    // A variable declaration consist of the optional input/output keywords,
+    // a type (int, bool, string or an array declaration e.g. int array[5] = {1,1,1,1,1}),
+    // a variable name (here A, B, and R)
+    input bool A, B, R
+    output bool O
+
+    // An anonymous region containing the logic of the SCChart.
+    // Regions are concurrently executed.
+    // A region can be declared via the region keyword and an options name.
+    // A region declaration looks like this: region A {<content>}
+    region {
+        // The initial state of this region.
+        // It is also an hierarchical state.
+        initial state ABO {
+            entry do O = false
+
+            initial state WaitAB {
+                region handleA {
+                    initial state wA
+                    if A go to dA
+
+                    final state dA
+                }
+
+                region handleB {
+                    initial state wB
+                    if B go to dB
+
+                    final state dB
+                }
+            }
+            do O = true join to done
+
+            state done
+        }
+        if R abort to ABO
+    }
+}`
+
+}
---
+++ keith-language/src/browser/keith-getting-started-widget.tsx
@@ -0,0 +1,109 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://rtsys.informatik.uni-kiel.de/kieler
+ *
+ * Copyright 2019 by
+ * + Kiel University
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ *
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ */
+
+import { GettingStartedWidget } from '@theia/getting-started/lib/browser/getting-started-widget';
+import * as React from 'react'
+import { OPEN_EXAMPLE_SCCHART } from './keith-getting-started-contribution';
+
+ export class KeithGettingStartedWidget extends GettingStartedWidget {
+
+    protected applicationName = 'KEITH';
+
+    protected keithDocumentationUrl = 'https://rtsys.informatik.uni-kiel.de/confluence/display/KIELER/KEITH';
+
+    protected runningKeithUrl = 'https://rtsys.informatik.uni-kiel.de/confluence/display/KIELER/Running+KEITH'
+
+    protected developingForKeithUrl = 'https://rtsys.informatik.uni-kiel.de/confluence/display/KIELER/Developing+for+KEITH+or+LS'
+
+    render() {
+        return <div className='gs-container'>
+            {this.renderHeader()}
+            <hr className='gs-hr' />
+            <div className='flex-grid'>
+                <div className='col'>
+                    {this.renderOpen()}
+                </div>
+            </div>
+            <div className='flex-grid'>
+                <div className='col'>
+                    {this.renderRecentWorkspaces()}
+                </div>
+            </div>
+            <div className='flex-grid'>
+                <div className='col'>
+                    {this.renderSettings()}
+                </div>
+            </div>
+            <div className='flex-grid'>
+                <div className='col'>
+                    {this.renderHelp()}
+                </div>
+            </div>
+            <div className='flex-grid'>
+                <div className='col'>
+                    {this.renderTutorial()}
+                </div>
+            </div>
+            <div className='flex-grid'>
+                <div className='col'>
+                    {this.renderVersion()}
+                </div>
+            </div>
+        </div>;
+    }
+
+    renderTutorial(): React.ReactNode {
+        return <div className='gs-section'>
+        <h3 className='gs-section-header'><i className='fa fa-info-circle'></i>Tutorial</h3>
+        <div className='gs-action-container'><a href='#' onClick={this.doOpenExampleSCChart}>Open example SCChart</a></div>
+    </div>;
+    }
+
+    protected renderHeader(): React.ReactNode {
+        return <div className='gs-header'>
+            <h1>{this.applicationName}<span className='gs-sub-header'> Getting Started</span></h1>
+        </div>;
+    }
+
+
+
+    protected renderHelp(): React.ReactNode {
+        return <div className='gs-section'>
+            <h3 className='gs-section-header'>
+                <i className='fa fa-question-circle'></i>
+                Help
+            </h3>
+            <div className='gs-action-container'>
+                <a href={this.documentationUrl} target='_blank'>Theia Documentation</a>
+            </div>
+            <div className='gs-action-container'>
+                <a href={this.keithDocumentationUrl} target='_blank'>KEITH Documentation</a>
+            </div>
+            <div className='gs-action-container'>
+                <a href={this.extensionUrl} target='_blank'>Building a New Extension</a>
+            </div>
+            <div className='gs-action-container'>
+                <a href={this.pluginUrl} target='_blank'>Building a New Plugin</a>
+            </div>
+            <div className='gs-action-container'>
+                <a href={this.runningKeithUrl} target='_blank'>Running KEITH (as a Developer)</a>
+            </div>
+            <div className='gs-action-container'>
+                <a href={this.developingForKeithUrl} target='_blank'>Developing for KEITH or KIELER LS</a>
+            </div>
+        </div>;
+    }
+
+    protected doOpenExampleSCChart = () => this.commandRegistry.executeCommand(OPEN_EXAMPLE_SCCHART.id);
+
+ }
