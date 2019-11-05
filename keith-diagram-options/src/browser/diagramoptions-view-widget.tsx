a36aa99b2c7da03674381dd684103da7cf402355
Niklas Rentz <nre@informatik.uni-kiel.de>
2019-11-05 17:07:38 +0100

    keith-diagram-options: added functionality for text options.

--- keith-diagram-options/src/browser/diagramoptions-view-widget.tsx
+++ keith-diagram-options/src/browser/diagramoptions-view-widget.tsx
@@ -159,6 +159,24 @@ export class DiagramOptionsViewWidget extends ReactWidget {
             }
         })
         // Render all top level options.
+        children.push(...this.renderOptions(optionsToRender))
+        if (children.length === 0) {
+            return undefined
+        } else {
+            return <div key='synthesisOptions'>
+                <p className='diagram-option seperator'>{'Diagram Options'}</p>
+                {...children}
+            </div>
+        }
+    }
+
+    /**
+     * Renders the given options, assuming that the categoryMap is already set up.
+     *
+     * @param optionsToRender The options that should be rendered now.
+     */
+    private renderOptions(optionsToRender: SynthesisOption[]): JSX.Element[] {
+        const children: JSX.Element[] = []
         optionsToRender.forEach(option => {
             switch (option.type) {
                 case TransformationOptionType.CHECK: {
@@ -171,6 +189,10 @@ export class DiagramOptionsViewWidget extends ReactWidget {
                 }
                 case TransformationOptionType.RANGE: {
                     children.push(this.renderRange(option as RangeOption))
+                    break
+                }
+                case TransformationOptionType.TEXT: {
+                    children.push(this.renderText(option))
                     break
                 }
                 case TransformationOptionType.SEPARATOR: {
@@ -186,14 +208,7 @@ export class DiagramOptionsViewWidget extends ReactWidget {
                 }
             }
         })
-        if (children.length === 0) {
-            return undefined
-        } else {
-            return <div key='synthesisOptions'>
-                <p className='diagram-option seperator'>{'Diagram Options'}</p>
-                {...children}
-            </div>
-        }
+        return children
     }
 
     /**
@@ -304,6 +319,35 @@ export class DiagramOptionsViewWidget extends ReactWidget {
     }
 
     /**
+     * Renders a text SynthesisOption as an HTML input with a text.
+     *
+     * @param option The text option to render.
+     */
+    private renderText(option: SynthesisOption): JSX.Element {
+        return <div key={option.sourceHash} className='diagram-option-text'>
+            <label htmlFor={option.name}>{option.name}:</label>
+            <input
+                type='text'
+                id={option.name}
+                name={option.name}
+                value={option.currentValue}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.onText(event, option)}
+            />
+        </div>
+    }
+
+    /**
+     * Called whenever a text option input field has been modified. Updates the option that belongs to this text field.
+     * @param event The input event that updated the text field.
+     * @param option The synthesis option connected to the text field.
+     */
+    private onText(event: React.ChangeEvent<HTMLInputElement>, option: SynthesisOption) {
+        option.currentValue = event.currentTarget.value
+        this.update()
+        this.sendNewSynthesisOption(option)
+    }
+
+    /**
      * Renders a separator SynthesisOption as an HTML label.
      *
      * @param option The separator option to render.
@@ -353,33 +397,7 @@ export class DiagramOptionsViewWidget extends ReactWidget {
      */
     private renderCategoryOptions(options: SynthesisOption[]): JSX.Element {
         let children: JSX.Element[] = []
-        options.forEach(option => {
-            switch (option.type) {
-                case TransformationOptionType.CHECK: {
-                    children.push(this.renderCheck(option))
-                    break
-                }
-                case TransformationOptionType.CHOICE: {
-                    children.push(this.renderChoice(option))
-                    break
-                }
-                case TransformationOptionType.RANGE: {
-                    children.push(this.renderRange(option as RangeOption))
-                    break
-                }
-                case TransformationOptionType.SEPARATOR: {
-                    children.push(this.renderSeperator(option))
-                    break
-                }
-                case TransformationOptionType.CATEGORY: {
-                    const list = this.categoryMap.get(option.name)
-                    if (list) {
-                        children.push(this.renderCategory(option, list))
-                    }
-                    break
-                }
-            }
-        })
+        children.push(...this.renderOptions(options))
         return <div className='category-options'>{...children}</div>
     }
 
