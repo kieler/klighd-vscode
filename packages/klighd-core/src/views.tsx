dcf7e441478c3f56561029380985a62b2b9ff79f
Christoph Fricke <stu209210@mail.uni-kiel.de>
2021-06-21 16:29:38 +0200

    feat(klighd-core): do not draw edges without source/target

--- packages/klighd-core/src/views.tsx
+++ packages/klighd-core/src/views.tsx
@@ -249,9 +249,14 @@ export class KEdgeView implements IView {
         edge.areChildAreaChildrenRendered = false
         edge.areNonChildAreaChildrenRendered = false
 
-        // edge should be greyed out if the source or target is moved
         const s = edge.source
         const t = edge.target
+        
+        // Do not draw edges without a source or target.
+        if (s === undefined || t === undefined) {
+            return <g/>
+        }
+        // edge should be greyed out if the source or target is moved
         if (s !== undefined && t !== undefined && s instanceof SKNode && t instanceof SKNode) {
             edge.moved = (s.selected || t.selected) && this.mListener.hasDragged
         }
