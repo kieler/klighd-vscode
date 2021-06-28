cbd3f69e178c90a3cc5898b451d9fd18e7b11777
Bennet Bleßmann <bennet.blessmann+github@googlemail.com>
2021-06-28 15:29:09 +0200

    Update packages/klighd-core/src/bookmarks/bookmark-panel.tsx
    apply suggestion
    
    Co-authored-by: Christoph Fricke <christoph@frickeonline.de>

--- packages/klighd-core/src/bookmarks/bookmark-panel.tsx
+++ packages/klighd-core/src/bookmarks/bookmark-panel.tsx
@@ -30,18 +30,12 @@ export class BookmarkPanel extends SidebarPanel {
         return '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bookmark"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"/></svg>'
     }
     render(): VNode {
-
-        const list: VNode[] = []
-
-        this.bookmarkRegistry.bookmarks.forEach((bookmark,index) => {
-            list.push(
-                <div>
+        return <div>{this.bookmarkRegistry.bookmarks.map((bookmark,index) => (
+                <div key={index}>
                     <button on-click={() => this.handleBookmarkActionClick(bookmark)}>{bookmark.name ?? ("Bookmark " + index)}</button>
                 </div>
             )
-        })
-
-        return  <div>{...list}</div>;
+        })}</div>;
     } 
 
     private handleBookmarkActionClick(bookmark: Bookmark) {
