a2de9e3d5c76ffab01f70daf31990bb9336e7ab7
Bennet Bleßmann <bb-github@t-online.de>
2021-07-05 15:06:16 +0200

    second attempt to fix copyright/license header

--- packages/klighd-core/src/depth-map.ts
+++ packages/klighd-core/src/depth-map.ts
@@ -1,10 +1,19 @@
 /*
-* This program and the accompanying materials are made available under the
-* terms of the Eclipse Public License 2.0 which is available at
-* http://www.eclipse.org/legal/epl-2.0.
-*
-* SPDX-License-Identifier: EPL-2.0
-*/
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://rtsys.informatik.uni-kiel.de/kieler
+ * 
+ * Copyright 2021 by
+ * + Kiel University
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 
 import { KNode, DetailLevel, DetailReference } from "klighd-interactive/lib/constraint-classes";
 import { VNode } from "snabbdom/vnode";
