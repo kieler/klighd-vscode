a3aebecb687d4b7c957d60fefe5e5d5a2646e7d7
fight4day <ti.ku10@web.de>
2022-08-30 13:35:49 +0200

    Revert "Hacky debug option for exporting diagram + proxy-view in current zoom + scroll"
    This reverts commit 9bb79d2489f05bd65bc980476dffb05d24d0fbf3.

--- packages/klighd-core/src/klighd-svg-exporter.ts
+++ packages/klighd-core/src/klighd-svg-exporter.ts
@@ -16,7 +16,7 @@
  */
 
 import { injectable } from "inversify";
-import { SGraph, SModelRoot, SvgExporter } from "sprotty";
+import { SModelRoot, SvgExporter } from "sprotty";
 import { RequestAction } from "sprotty-protocol";
 import { KlighdExportSvgAction } from "./actions/actions";
 
@@ -27,53 +27,16 @@ export class KlighdSvgExporter extends SvgExporter {
 
     export(root: SModelRoot, request?: RequestAction<KlighdExportSvgAction>): void {
         if (typeof document !== 'undefined') {
-            const whole = false;
-
-            if (whole) {
-                const div = document.getElementById(this.options.hiddenDiv);
-                if (div !== null && div.firstElementChild && div.firstElementChild.tagName === 'svg') {
-                    const svgElement = div.firstElementChild as SVGSVGElement;
-                    const svg = this.createSvg(svgElement, root);
-                    this.actionDispatcher.dispatch(KlighdExportSvgAction.create(svg, request ? request.requestId : '', root.id));
-                }
-            } else {
-                // FIXME: restore this, only for exporting with proxy-view
-                const div = document.getElementById("keith-diagram_sprotty") //(this.options.hiddenDiv);
-                // Create new svg with proxy-view and diagram as children
-                const temp = document.createElement("svg") as Element;
-                temp.id = "TEMP_ID";
-                temp.className = "sprotty-graph";
-
-                // <g>s on same level
-                temp.appendChild(document.getElementById(this.options.hiddenDiv)!.firstElementChild!.firstChild!.cloneNode());
-                Array.of(
-                    // Sprotty graph
-                    ...Array.from(div!.lastElementChild!.children),
-                    // Proxy-view
-                    ...Array.from(div!.firstElementChild!.firstElementChild!.children)
-                ).forEach(c => temp.firstChild!.appendChild(c.cloneNode(true)));
-
-                console.log(temp)
-
-                // Just the sidebar
-                // temp.appendChild(document.getElementById("keith-diagram_sprotty_sidebar")!);
-
-                // Nested SVGs
-                // temp.appendChild(div!.firstElementChild!.firstElementChild!.cloneNode(true));
-                // temp.appendChild(div!.lastElementChild!.cloneNode(true));
-
-                // Put everything in sprotty-graph svg
-                // const temp = div!.lastElementChild!.cloneNode(true);
-                // Array.from(div!.firstElementChild!.firstElementChild!.children).forEach(e => temp.firstChild!.appendChild(e.cloneNode(true)));
-
-                const svgElement = temp as SVGSVGElement;
-                const svg = this.createSvg(svgElement, root, whole);
+            const div = document.getElementById(this.options.hiddenDiv);
+            if (div !== null && div.firstElementChild && div.firstElementChild.tagName === 'svg') {
+                const svgElement = div.firstElementChild as SVGSVGElement;
+                const svg = this.createSvg(svgElement, root);
                 this.actionDispatcher.dispatch(KlighdExportSvgAction.create(svg, request ? request.requestId : '', root.id));
             }
         }
     }
 
-    protected createSvg(svgElementOrig: SVGSVGElement, root: SModelRoot, whole = true): string {
+    protected createSvg(svgElementOrig: SVGSVGElement, root: SModelRoot): string {
         const serializer = new XMLSerializer();
         const svgCopy = serializer.serializeToString(svgElementOrig);
         const iframe: HTMLIFrameElement = document.createElement('iframe');
@@ -89,7 +52,7 @@ export class KlighdSvgExporter extends SvgExporter {
         svgElementNew.setAttribute('version', '1.1');
         // Somehow this is always 1.
         svgElementNew.setAttribute('opacity', '1');
-        const bounds = whole ? this.getBounds(root) : { ...(root as SGraph).canvasBounds, ...(root as SGraph).scroll };
+        const bounds = this.getBounds(root);
         svgElementNew.setAttribute('viewBox', `${bounds.x} ${bounds.y} ${bounds.width} ${bounds.height}`);
         const svgCode = serializer.serializeToString(svgElementNew);
         document.body.removeChild(iframe);
@@ -101,7 +64,7 @@ export class KlighdSvgExporter extends SvgExporter {
             if (!skipedProperties.includes(key)) {
                 const value = source.getAttribute(key)
                 if (value)
-                    target.setAttribute(key, value)
+                target.setAttribute(key, value)
             }
         })
         // IE doesn't retrun anything on source.children
