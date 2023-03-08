2430a753d469702d78bc7c758a20d68a3c85dbf3
Max Kasperowski <mka@informatik.uni-kiel.de>
2023-03-08 13:46:26 +0100

    add example unit tests

---
+++ packages/klighd-core/test/diagram-pieces/diagram-piece-request-manager.test.ts
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import { GridDiagramPieceRequestManager } from '../../src/diagram-pieces/diagram-piece-request-manager';
+
+describe('coordinate-integer-conversion', () => {
+    it('getKey encodes coordinates in a single 32 bit encoding', () => {
+        const manager = new GridDiagramPieceRequestManager();
+        const key = manager.getKey({ x: 0, y: 0 });
+        expect(key).to.equal(0);
+
+        const key2 = manager.getKey({ x: 32766, y: 32766 });
+        expect(key2).to.equal(2147352576 + 32766);
+    });
+
+    it('getKey throws an error if coordinates are larger than 16 bit unsigned integers', () => {
+        const manager = new GridDiagramPieceRequestManager();
+        expect(() => manager.getKey({ x: 32768, y: 32768 })).to.throw;
+    });
+
+    it('getCoords decodes a 32 bit integer into two 16 bit coordinates', () => {
+        const manager = new GridDiagramPieceRequestManager();
+        const coords = manager.getCoords(2147352576 + 32766);
+        expect(coords).to.deep.equal({ x: 32766, y: 32766 });
+
+        const coords2 = manager.getCoords(0);
+        expect(coords2).to.deep.equal({ x: 0, y: 0 });
+    });
+
+    it('getCoords throws an error if the integer is larger than 32 bit', () => {
+        const manager = new GridDiagramPieceRequestManager();
+        expect(manager.getCoords(2147352576 + 32766 + 1)).to.throw;
+    });
+});
